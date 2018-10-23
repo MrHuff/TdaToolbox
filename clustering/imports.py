49813392b96fd04c2969a9ce3716df7732375af6
unknown <526473+gb96@users.noreply.github.com>
2018-10-23 23:39:10 +0100

    using conda instead

--- clustering/imports.py
+++ clustering/imports.py
@@ -3,24 +3,12 @@
 # Project: TDAToolbox
 
 import sys
-
 import numpy as np
-
 from scipy.stats import kde
 from sklearn.neighbors import KDTree
 from sklearn.datasets import make_blobs, make_circles, make_moons
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gds
+from mpl_toolkits.mplot3d import Axes3D
+import gudhi
 
-try:
-	import matplotlib.pyplot as plt
-	import matplotlib.gridspec as gds
-	from mpl_toolkits.mplot3d import Axes3D
-except:
-	print('[###] Visual packages could not be imported ...')
-
-try: 
-	gpath = '2018-01-31-09-25-53_GUDHI_2.1.0/build/cython'
-	build = '/home/intern/Downloads/{}'.format(gpath)
-	sys.path.append(build)
-	import gudhi
-except:
-	print('[###] Gudhi could not be imported ...')
--- filtration/imports.py
+++ filtration/imports.py
@@ -6,22 +6,12 @@ import sys
 
 import numpy as np
 
-from sklearn.neighbors import KDTree
+from sklearn.neighbors import KDTree,cKDTree
 
-try:
-    import matplotlib.pyplot as plt
-    import matplotlib.gridspec as gds
-    import plotly.offline as pyo
-    import plotly.graph_objs as go
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gds
+import plotly.offline as pyo
+import plotly.graph_objs as go
+from plotly import tools
+import gudhi
 
-    from plotly import tools
-except:
-    print('[###] Visual packages could not be imported ...')
-
-try: 
-    gpath = '2018-01-31-09-25-53_GUDHI_2.1.0/build/cython'
-    build = '/home/intern/Downloads/{}'.format(gpath)
-    sys.path.append(build)
-    import gudhi
-except:
-    print('[###] Gudhi could not be imported ...')
